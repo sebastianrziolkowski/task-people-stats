34c69ff2bff48f82d183f90928c958fa1a7f366b
Sebastian Ziolkowski <osomjokey2@gmail.com>
2020-10-21 16:43:17 +0200

    update Person

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
 
-public class Main {
-
+class Main {
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello world");
+    }
+}
+
+class PeopleStats {
+
+}
+
+class Person {
+    private final String firstName;
+    private final String lastName;
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 }
