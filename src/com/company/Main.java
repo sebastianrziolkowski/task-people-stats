443ca93c8caa21c06bedbcbf12f231b8fb661902
Sebastian Ziolkowski <osomjokey2@gmail.com>
2020-10-21 16:46:48 +0200

    update peopleStatsv3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,6 +12,7 @@ class Main {
         PeopleStats peopleStats = new PeopleStats(Paths.get("śćieżka", "do", "pliku"));
 
         System.out.println(String.format("Liczba osób: %d", peopleStats.count()));
+        System.out.println(String.format("Liczba osób z unikalnymi nazwiskami: %d", peopleStats.countUniqueLastNames()));
     }
 
 }
@@ -31,6 +32,13 @@ class PeopleStats {
     }
     public long count() {
         return people.size();
+    }
+
+    public long countUniqueLastNames() {
+        return people.stream()
+                .map(Person::getLastName)
+                .distinct()
+                .count();
     }
 }
 
