978f0baebfb3e7771eca9b54dffac4e11e74eee5
Sebastian Ziolkowski <osomjokey2@gmail.com>
2020-10-21 16:44:09 +0200

    update peopleStats

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,11 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
@@ -7,7 +13,18 @@ class Main {
 }
 
 class PeopleStats {
+    private final List<Person> people;
 
+    public PeopleStats(Path inputFilePath) {
+        try {
+            people = Files.lines(inputFilePath)
+                    .map(line -> line.split("\t"))
+                    .map(chunks -> new Person(chunks[0], chunks[1]))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 }
 
 class Person {
@@ -26,3 +43,4 @@ class Person {
         return lastName;
     }
 }
+
